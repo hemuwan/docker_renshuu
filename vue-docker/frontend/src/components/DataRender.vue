9f538b7eaf1a09011d8a2c4c5a364306ded4edf5
ubuntu-hemuwan <sukeakorou@yahoo.co.jp>
2026-02-10 08:43:40 +0900

    [add] vite vue container

---
+++ vue-docker/frontend/src/components/DataRender.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const tableData = ref([
+    {
+      id: 1
+      , title: 'title:1'
+    }
+    ,{
+      id: 2
+      , title: 'title:2'
+    }
+  ]);
+
+  const push = () => {
+    tableData.value.push({
+      id: tableData.value.at(-1).id + 1
+      , title: 'pushed!'
+    });
+  }
+  const change = () => {
+    tableData.value[1].title = 'change!'
+  }
+</script>
+
+<template>
+  <p>hei</p>
+  <div v-for="node in tableData" :key="node.id">
+    {{ node.title }}
+    <button @click="push">click!</button>
+  </div>
+  <button @click="push">click!</button>
+  <button @click="change">change!</button>
+</template>
